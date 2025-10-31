9fd568a22b370f3ee433a69170823f1ee6f428ea
ISHANK1313 <ishankp3@gmail.com>
2025-10-31 23:05:27 +0530

    FIXED: Increase length to handle long titles

--- src/main/java/com/cinelog/CineLog/entity/WatchlistItem.java
+++ src/main/java/com/cinelog/CineLog/entity/WatchlistItem.java
@@ -5,88 +5,98 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
+@Entity
+public class WatchlistItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Entity
-    public class WatchlistItem {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
+    private User user;
 
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "user_id", nullable = false)
-        @JsonIgnore
-        private User user;
+    private Long tmdbMovieId;
 
-        private Long tmdbMovieId;
-        private String title;
-        @Column(columnDefinition = "TEXT")
-        private String overview;
-        private String posterPath;
-        private String releaseDate;
-        private LocalDateTime addedAt;
+    // FIXED: Increase length to handle long titles
+    @Column(length = 500)
+    private String title;
 
-        public Long getId() {
-            return id;
-        }
+    @Column(columnDefinition = "TEXT")
+    private String overview;
 
-        public void setId(Long id) {
-            this.id = id;
-        }
+    // FIXED: Poster paths can be very long URLs
+    @Column(length = 1000)
+    private String posterPath;
 
-        public User getUser() {
-            return user;
-        }
+    @Column(length = 50)
+    private String releaseDate;
 
-        public void setUser(User user) {
-            this.user = user;
-        }
+    private LocalDateTime addedAt;
 
-        public Long getTmdbMovieId() {
-            return tmdbMovieId;
-        }
 
-        public void setTmdbMovieId(Long tmdbMovieId) {
-            this.tmdbMovieId = tmdbMovieId;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public String getOverview() {
-            return overview;
-        }
-
-        public void setOverview(String overview) {
-            this.overview = overview;
-        }
-
-        public String getPosterPath() {
-            return posterPath;
-        }
-
-        public void setPosterPath(String posterPath) {
-            this.posterPath = posterPath;
-        }
-
-        public String getReleaseDate() {
-            return releaseDate;
-        }
-
-        public void setReleaseDate(String releaseDate) {
-            this.releaseDate = releaseDate;
-        }
-
-        public LocalDateTime getAddedAt() {
-            return addedAt;
-        }
-
-        public void setAddedAt(LocalDateTime addedAt) {
-            this.addedAt = addedAt;
-        }
+    public Long getId() {
+        return id;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getTmdbMovieId() {
+        return tmdbMovieId;
+    }
+
+    public void setTmdbMovieId(Long tmdbMovieId) {
+        this.tmdbMovieId = tmdbMovieId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public LocalDateTime getAddedAt() {
+        return addedAt;
+    }
+
+    public void setAddedAt(LocalDateTime addedAt) {
+        this.addedAt = addedAt;
+    }
+}
 
